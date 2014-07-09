67e5390d53a62f9eb3bfe0688577fd3ea813e576
Tiensbakung <Tiensbakung@gmail.com>
2014-07-09 16:41:46 +0200

    regular emacs package udpates.

---
+++ emacs.d/elpa/flx-20140630.2152/flx-autoloads.el
@@ -0,0 +1,18 @@
+;;; flx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("flx-pkg.el" "flx.el") (21437 21444 692616
+;;;;;;  97000))
+
+;;;***
+
+(provide 'flx-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flx-autoloads.el ends here

9baa76eb1757cab3f3529687b96364ab0a2bd2f4
Tiensbakung <Tiensbakung@gmail.com>
2014-09-13 00:06:49 +0200

    removed emacs marmalade repo.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,7 +3,7 @@
 ;;; Personal Emacs init.el
 ;;; Code:
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 ;; set emacs encoding

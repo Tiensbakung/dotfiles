464186a50e25318ce57539021f608aeea7ac90fc
Tiensbakung <Tiensbakung@gmail.com>
2015-02-06 17:27:13 +0100

    Regular emacs package updates.

--- emacs.d/elpa/font-lock+-20140831.615/font-lock+-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "font-lock+" "20140831.615" "Enhancements to standard library `font-lock.el'." (quote nil))
---
+++ emacs.d/elpa/font-lock+-20150104.2205/font-lock+-pkg.el
@@ -0,0 +1,1 @@
+(define-package "font-lock+" "20150104.2205" "Enhancements to standard library `font-lock.el'." 'nil :url "http://www.emacswiki.org/font-lock+.el" :keywords '("languages" "faces" "highlighting"))

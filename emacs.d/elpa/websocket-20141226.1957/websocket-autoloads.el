ea5714ceefc98f822cfb830fea506b7e71f75fff
Tiensbakung <Tiensbakung@gmail.com>
2015-04-18 20:22:44 +0200

    emacs:regular package updates.

--- emacs.d/elpa/websocket-20141226.1957/websocket-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; websocket-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("websocket.el") (21716 58882 569495 227000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; websocket-autoloads.el ends here

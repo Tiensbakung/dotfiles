b90e432af90c74e4de89d9b0f3ae25f9234cd569
Tiensbakung <Tiensbakung@gmail.com>
2015-05-16 08:43:15 +0200

    emacs: regular package updates.

--- emacs.d/elpa/fcitx-20150417.1052/fcitx-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "fcitx" "20150417.1052" "Make fcitx better in Emacs" 'nil :keywords '("extensions"))
---
+++ emacs.d/elpa/fcitx-20150514.1257/fcitx-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fcitx" "20150514.1257" "Make fcitx better in Emacs" 'nil :keywords '("extensions"))

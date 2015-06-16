e95c3eccb7787058f0bda431d1bef2b287d5957a
Tiensbakung <Tiensbakung@gmail.com>
2015-06-16 12:35:26 +0200

    emacs: regular package updates.

--- emacs.d/elpa/fcitx-20150521.1012/fcitx-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "fcitx" "20150521.1012" "Make fcitx better in Emacs" 'nil :url "https://github.com/cute-jumper/fcitx.el" :keywords '("extensions"))
---
+++ emacs.d/elpa/fcitx-20150610.1953/fcitx-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fcitx" "20150610.1953" "Make fcitx better in Emacs" 'nil :url "https://github.com/cute-jumper/fcitx.el" :keywords '("extensions"))

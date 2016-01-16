908cf9727d1d2197878e2ae9d2f9f5943b589af3
Tiensbakung <Tiensbakung@gmail.com>
2016-01-16 16:34:44 +0100

    emacs/elpa: regular update

---
+++ emacs.d/elpa/concurrent-20160109.2046/concurrent-autoloads.el
@@ -0,0 +1,16 @@
+;;; concurrent-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("concurrent-pkg.el" "concurrent.el") (22170
+;;;;;;  25299 47845 10000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; concurrent-autoloads.el ends here
---
+++ emacs.d/elpa/fcitx-20160107.2118/fcitx-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fcitx" "20160107.2118" "Make fcitx better in Emacs" 'nil :url "https://github.com/cute-jumper/fcitx.el" :keywords '("extensions"))

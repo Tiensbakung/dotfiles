eb3f39b4038c8032c9c63a290b502af01961a997
Tiensbakung <Tiensbakung@gmail.com>
2016-01-23 16:54:42 +0100

    emacs/dir-locales.el: add flycheck-clang-include-path and flycheck-gcc-include-path

--- dir-locals.el
+++ dir-locals.el
@@ -1,2 +1,11 @@
-((nil . ((company-clang-arguments . ("-I/project_root/include1/"
-                                     "-I/project_root/include2/")))))
+((nil .
+      ((company-clang-arguments . ("-Isrc"
+                                   "-I/tmp/src/smartrest/include"
+                                   "-I/tmp/src/smartrest/src"))
+       (flycheck-clang-include-path
+        . ("/tmp/src/smartrest/include"
+           "/tmp/src/smartrest/ext/LuaBridge/Source/LuaBridge"))
+       (flycheck-gcc-include-path
+        . ("/tmp/src/smartrest/include"
+           "/tmp/src/smartrest/ext/LuaBridge/Source/LuaBridge"))
+       )))

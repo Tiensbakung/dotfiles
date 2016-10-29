4720e036a44823745a8091b4f484b2a211eaa3be
Tiensbakung <Tiensbakung@gmail.com>
2016-10-29 16:26:22 +0200

    emacs/elpa: package update.

--- emacs.d/elpa/kivy-mode-20160623.1210/kivy-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "kivy-mode" "20160623.1210" "Emacs major mode for editing Kivy files" 'nil)
---
+++ emacs.d/elpa/kivy-mode-20160902.1041/kivy-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kivy-mode" "20160902.1041" "Emacs major mode for editing Kivy files" 'nil)

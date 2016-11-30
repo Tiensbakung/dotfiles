d634fcae27cded255a4b60013533cefdbc4e3e13
Tiensbakung <Tiensbakung@gmail.com>
2016-11-30 14:32:31 +0100

    emacs/elpa: update, remove autopair, rainbow, font-lock+

--- emacs.d/elpa/font-lock+-20151231.1319/font-lock+-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; font-lock+-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("font-lock+-pkg.el" "font-lock+.el") (22170
-;;;;;;  25288 45064 13000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; font-lock+-autoloads.el ends here

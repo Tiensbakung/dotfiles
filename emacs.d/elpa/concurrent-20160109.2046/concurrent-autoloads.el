5826d4dd89ee065580779b89a7ee718e76a51899
Tiensbakung <Tiensbakung@gmail.com>
2017-01-16 11:08:37 +0100

    emacs.d/elpa: package update.

--- emacs.d/elpa/concurrent-20160109.2046/concurrent-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; concurrent-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("concurrent-pkg.el" "concurrent.el") (22170
-;;;;;;  25299 47845 10000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; concurrent-autoloads.el ends here
--- emacs.d/elpa/deferred-20160109.2046/deferred-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; deferred-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("deferred-pkg.el" "deferred.el") (22170
-;;;;;;  25298 244248 623000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; deferred-autoloads.el ends here

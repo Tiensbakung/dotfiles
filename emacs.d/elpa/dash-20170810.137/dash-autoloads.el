435c68e77d11e33f741de55b011d13920fa3004d
Tiensbakung <Tiensbakung@gmail.com>
2017-09-19 00:46:56 +0800

    emacs.d/elpa: package updates.

---
+++ emacs.d/elpa/dash-20170810.137/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash.el") (22975 63352 604054 162000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here

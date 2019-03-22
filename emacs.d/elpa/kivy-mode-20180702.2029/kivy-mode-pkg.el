263fdc1d130051d213727a2df57cf4d9df1af971
Tiensbakung <Tiensbakung@gmail.com>
2019-03-22 19:37:48 +0800

    emacs.d/elpa: remove python related packages.

--- emacs.d/elpa/kivy-mode-20180702.2029/kivy-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "kivy-mode" "20180702.2029" "Emacs major mode for editing Kivy files" 'nil :commit "11c9fe6c03fa03d16eceb1656ca51ff0129815dc" :authors '(("Dean Serenevy" . "dean@serenevy.net")) :maintainer '("Dean Serenevy" . "dean@serenevy.net"))

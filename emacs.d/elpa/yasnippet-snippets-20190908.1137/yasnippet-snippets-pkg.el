8d198dc991c356a061565f0663d7ce8dba9e7170
Tiensbakung <Tiensbakung@gmail.com>
2019-09-17 18:09:07 +0800

    emacs.d/elpa: package updates.

---
+++ emacs.d/elpa/yasnippet-snippets-20190908.1137/yasnippet-snippets-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yasnippet-snippets" "20190908.1137" "Collection of yasnippet snippets" '((yasnippet "0.8.0")) :commit "676a6ddc190fb957dca3c42a777e34a623f15cbd" :keywords '("snippets") :authors '(("Andrea Crotti" . "andrea.crotti.0@gmail.com")) :maintainer '("Andrea Crotti" . "andrea.crotti.0@gmail.com"))

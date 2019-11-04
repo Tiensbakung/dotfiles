d4066a8210f96fd4794305df568fd9a0297f20f6
Tiensbakung <Tiensbakung@gmail.com>
2019-11-04 09:06:04 +0800

    emacs.d/elpa: package updates

---
+++ emacs.d/elpa/counsel-20191103.537/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20191103.537" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "43daeb1cd1edbcc00bdfae1d9ddb49b368ebe99f" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")

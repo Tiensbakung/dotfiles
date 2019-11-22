194255b5b97f6963823be6d11eae73d36ed6a1be
Tiensbakung <Tiensbakung@gmail.com>
2019-11-22 10:38:19 +0800

    emacs.d/elpa: package updates.

---
+++ emacs.d/elpa/lv-20191106.1238/lv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lv" "20191106.1238" "Other echo area" 'nil :commit "d38c63d3d85a06fd34b28626d7ebd97bb05db48c" :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel"))

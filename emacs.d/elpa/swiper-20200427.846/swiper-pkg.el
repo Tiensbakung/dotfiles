20473efd70b40a6833c781a8ff08118e0fea9b55
Tiensbakung <Tiensbakung@gmail.com>
2020-05-17 23:22:26 +0800

    emacs.d/elpa: package update.s

--- emacs.d/elpa/swiper-20200427.846/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20200427.846" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "c5517fc3592bcd89e9e1dd3fa203a4e3418640df" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")

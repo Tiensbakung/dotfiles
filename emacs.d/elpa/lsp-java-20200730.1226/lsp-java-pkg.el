836ee1af88445856e3387df2d790f16184430c91
Tiensbakung <Tiensbakung@gmail.com>
2020-07-31 10:07:20 +0800

    emacs.d/elpa: package updates.

---
+++ emacs.d/elpa/lsp-java-20200730.1226/lsp-java-pkg.el
@@ -0,0 +1,17 @@
+(define-package "lsp-java" "20200730.1226" "Java support for lsp-mode"
+  '((emacs "25.1")
+    (lsp-mode "6.0")
+    (markdown-mode "2.3")
+    (dash "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (dash-functional "1.2.0")
+    (request "0.3.0")
+    (treemacs "2.5")
+    (dap-mode "0.5"))
+  :commit "74418b853f4281c331b55b6b4bf1839fe5acf117" :keywords
+  '("languague" "tools")
+  :url "https://github.com/emacs-lsp/lsp-java")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

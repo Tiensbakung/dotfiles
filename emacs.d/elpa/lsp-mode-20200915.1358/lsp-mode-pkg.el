1f25ea663f6cad4e36ef795be7f680e66e4b95e3
Tiensbakung <Tiensbakung@gmail.com>
2020-09-16 09:33:20 +0800

    emacs.d/elpa: package updates.

--- emacs.d/elpa/lsp-mode-20200915.1358/lsp-mode-pkg.el
+++ emacs.d/elpa/lsp-mode-20200915.1358/lsp-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-mode" "20200915.532" "LSP mode"
+(define-package "lsp-mode" "20200915.1358" "LSP mode"
   '((emacs "26.1")
     (dash "2.14.1")
     (dash-functional "2.14.1")
@@ -7,7 +7,7 @@
     (spinner "1.7.3")
     (markdown-mode "2.3")
     (lv "0"))
-  :commit "d5f5c97bf1894b6e1dca0f3146aeaca069be50f0" :keywords
+  :commit "094dd398d302218aad41afd0ca1c31f47162848f" :keywords
   '("languages")
   :authors
   '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))

77c17c8f12e7bfc47b444dd457d62d9b11ab634e
Tiensbakung <Tiensbakung@gmail.com>
2020-10-28 16:48:25 +0800

    emacs.d/elpa: package updates.

---
+++ emacs.d/elpa/lsp-mode-20201027.2011/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20201027.2011" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "cf1bb2d4753f778035f351992e7b02183c13c0a2" :keywords
+  '("languages")
+  :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

e20190172f3475ef72ccca793aeab77831ba2aa7
Tiensbakung <Tiensbakung@gmail.com>
2021-07-06 14:39:59 +0800

    emacs.d/elpa: package updates.

---
+++ emacs.d/elpa/w3m-20210615.103/w3m-pkg.el
@@ -0,0 +1,5 @@
+(define-package "w3m" "20210615.103" "an Emacs interface to w3m" 'nil :commit "c01f5c4da8883df76ffcb35a5e2d000336203289" :keywords
+  '("w3m" "www" "hypermedia"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
